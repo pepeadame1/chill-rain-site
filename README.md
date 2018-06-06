78b208eeab570e10708560d6187d0da82974c991
Jose Adame <huajuco4@gmail.com>
2018-06-05 20:41:32 -0500

    Added the initial commit
    The original idea was to implement a parallax effect that is commented
    on the app.js because it simply didnt look good

---
+++ README.md
@@ -0,0 +1,1 @@
+# chill-rain-site
---
+++ app.js
@@ -0,0 +1,206 @@
+var app = new PIXI.Application({
+    autoResize: true,
+    resolution: devicePixelRatio,
+    backgroundColor : 0x1099bb
+});
+
+document.body.appendChild(app.view);
+//agregar background
+var background = PIXI.Sprite.fromImage('resources/background.png');
+// center the sprite anchor point
+background.anchor.x = 0;
+background.anchor.y = 0;
+
+background.position.x = -40;
+background.position.y = 0;
+
+
+app.stage.addChild(background);
+
+//agregar musica y sonido de lluvia
+var audio = new Audio('resources/rain.mp3');
+var song = new Audio('resources/song.mp3');
+audio.loop = true;
+song.loop = true;
+audio.play();
+song.play();
+var drops = [];
+var drops2 = [];
+var drops3 = [];
+var drops4 = [];
+
+//resize the screen to the correct size
+window.addEventListener('resize', resize);
+function resize() {
+    repeat();
+	// Resize the renderer
+	app.renderer.resize(window.innerWidth, window.innerHeight);
+  
+  // You can use the 'screen' property as the renderer visible
+  // area, this is more useful than view.width/height because
+  // it handles resolution
+  rect.position.set(app.screen.width, app.screen.height);
+  
+}
+
+//for loop para iniciar todas las primeras lluvias
+var ranX;
+var ranY;
+var i;
+for (i = 0; i < 300; i++) {
+    ranX = Math.floor((Math.random() * app.screen.width));
+    ranY = Math.floor((Math.random() * app.screen.height/3) + 1);
+    drops[i] = new PIXI.Graphics();
+    drops[i].beginFill(0x004BBC);
+    drops[i].drawRect(ranX,ranY,4,20);
+    app.stage.addChild(drops[i]);
+}
+
+//hacer gotas layer 2
+var ranX;
+var ranY;
+var i;
+for (i = 0; i < 400; i++) {
+    ranX = Math.floor((Math.random() * app.screen.width));
+    ranY = Math.floor((Math.random() * app.screen.height/3) + 1);
+    drops2[i] = new PIXI.Graphics();
+    drops2[i].beginFill(0x214887);
+    drops2[i].drawRect(ranX,ranY,3,15);
+    app.stage.addChild(drops2[i]);
+}
+
+//hacer gotas layer 3
+var ranX;
+var ranY;
+var i;
+for (i = 0; i < 500; i++) {
+    ranX = Math.floor((Math.random() * app.screen.width));
+    ranY = Math.floor((Math.random() * app.screen.height/3) + 1);
+    drops3[i] = new PIXI.Graphics();
+    drops3[i].beginFill(0x214887);
+    drops3[i].drawRect(ranX,ranY,2,10);
+    app.stage.addChild(drops3[i]);
+}
+
+//hacer gotas layer 4
+var ranX;
+var ranY;
+var i;
+for (i = 0; i < 600; i++) {
+    ranX = Math.floor((Math.random() * app.screen.width));
+    ranY = Math.floor((Math.random() * app.screen.height/3) + 1);
+    drops4[i] = new PIXI.Graphics();
+    drops4[i].beginFill(0x214887);
+    drops4[i].drawRect(ranX,ranY,1,5);
+    app.stage.addChild(drops4[i]);
+}
+
+
+
+app.ticker.add(function(delta){
+
+var mouseposition = app.renderer.plugins.interaction.mouse.global;
+shift = mouseposition.x;
+//drops1
+var i;
+for (i = 0; i < 300; i++) {
+    //drops[i].x = (shift-app.screen.width/2)*.25;
+    speed = i/10;
+    if(speed<30){
+        speed = speed+5;
+    }
+    drops[i].y=drops[i].y+speed;
+    if(drops[i].y>app.screen.height){
+        drops[i].y=-30;
+    }
+}
+//drops2
+var i;
+for (i = 0; i < 400; i++) {
+    //drops2[i].x = (shift-app.screen.width/2)*.5;
+    speed = i/15;
+    if(speed<30){
+        speed = speed+5;
+    }
+    drops2[i].y=drops2[i].y+speed;
+    if(drops2[i].y>app.screen.height){
+        drops2[i].y=-30;
+    }
+}
+//drops3
+var i;
+for (i = 0; i < 500; i++) {
+    //drops3[i].x = (shift-app.screen.width/2)*.75;
+    speed = i/15;
+    if(speed<30){
+        speed = speed+5;
+    }
+    drops3[i].y=drops3[i].y+speed;
+    if(drops3[i].y>app.screen.height){
+        drops3[i].y=-30;
+    }
+}
+//drops4
+var i;
+for (i = 0; i < 600; i++) {
+    //drops4[i].x = (shift-app.screen.width/2)*1;
+    speed = i/50;
+    if(speed<30){
+        speed = speed+5;
+    }
+    drops4[i].y=drops4[i].y+speed;
+    if(drops4[i].y>app.screen.height){
+        drops4[i].y=-30;
+    }
+}
+//move background
+//background.position.x = -100+(shift-app.screen.width/2)*.10;
+
+});
+
+function repeat(){
+
+//for loop para iniciar todas las primeras lluvias
+var ranX;
+var ranY;
+var i;
+for (i = 0; i < 300; i++) {
+    ranX = Math.floor((Math.random() * app.screen.width)-150);
+    ranY = Math.floor((Math.random() * app.screen.height/3) + 1);
+    drops[i].x = ranX;
+    drops[i].y = ranY;
+}
+
+//hacer gotas layer 2
+var ranX;
+var ranY;
+var i;
+for (i = 0; i < 400; i++) {
+    ranX = Math.floor((Math.random() * app.screen.width)-150);
+    ranY = Math.floor((Math.random() * app.screen.height/3) + 1);
+    drops2[i].x = ranX;
+    drops2[i].y = ranY;
+}
+
+//hacer gotas layer 3
+var ranX;
+var ranY;
+var i;
+for (i = 0; i < 500; i++) {
+    ranX = Math.floor((Math.random() * app.screen.width)-150);
+    ranY = Math.floor((Math.random() * app.screen.height/3) + 1);
+    drops3[i].x = ranX;
+    drops3[i].y = ranY;
+}
+
+//hacer gotas layer 4
+var ranX;
+var ranY;
+var i;
+for (i = 0; i < 600; i++) {
+    ranX = Math.floor((Math.random() * app.screen.width)-150);
+    ranY = Math.floor((Math.random() * app.screen.height/3) + 1);
+    drops4[i].x = ranX;
+    drops4[i].y = ranY;
+}
+};
